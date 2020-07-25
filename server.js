cd4dedb0c2cb2d15440c182954d9077ed9652633
begumsacak <63175082+begumsacak@users.noreply.github.com>
2020-07-25 00:02:39 -0500

    get request for notes.html fixed

--- server.js
+++ server.js
@@ -7,16 +7,15 @@ const fs = require("fs")
 //The Path module provides a way of working with directories and file paths.
 const path = require("path")
 
-
-
 //adding routes
-// respond with "notes.html" when a GET request is made to the homepage
+// respond with "index.html" when a GET request is made to the homepage
 //__dirname tells us the absolute path of the directory containing the currently executing file (from https://www.digitalocean.com/community/tutorials/nodejs-how-to-use__dirname)
 app.get("/", (req, res) => {
     res.sendFile((path.join)(__dirname, "public", "index.html"));
 });
 
-app.get("/", (req, res) => {
+// respond with "notes.html" when a GET request is made to the homepage
+app.get("/notes", (req, res) => {
     res.sendFile((path.join)(__dirname, "public", "notes.html"));
 });
 
